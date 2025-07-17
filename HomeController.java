bad609330c196c496ae4612b9fbd1c7a85860d51
robert1357 <roberth.gonzales@ucsp.edu.pe>
2025-07-17 04:03:04 -0500

    Create HomeController.java

---
+++ HomeController.java
@@ -0,0 +1,13 @@
+package com.tuapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "✅ Backend funcionando correctamente en Railway 🚀";
+    }
+}
